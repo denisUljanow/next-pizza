b533a530ad5dda3914814bcdc358922f7ca3653c
dul <denisuljanow@gmail.com>
2025-10-10 13:56:40 +0200

    Ingridients window added

--- components/shared/checkbox-filters-group.tsx
+++ components/shared/checkbox-filters-group.tsx
@@ -10,7 +10,7 @@ interface Props {
   className?: string;
   title: string;
   items: Item[];
-  defaultItems?: Item[];
+  defaultItems: Item[];
   limit?: number;
   onChange?: (values: string[]) => void;
   defaultValue?: string[];
@@ -22,19 +22,34 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
   items,
   defaultItems,
   limit = 5,
-  searchInputPlaceholder = 'Поиск...',
+  searchInputPlaceholder = 'Suche...',
   className,
   onChange,
   defaultValue,
 }) => {
+  const [showAll, setShowAll] = React.useState(false);
+  const [searchVal, setSearchVal] = React.useState('');
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchVal(e.target.value);
+  };
+  const list = showAll
+    ? items.filter((item) => item.text.toLowerCase().includes(searchVal.toLowerCase()))
+    : defaultItems.slice(0, limit);
   return (
     <div className={className}>
       <p className="font-bold mb-3">{title}</p>
-      <div className="mb-5">
-        <Input placeholder={searchInputPlaceholder} className="bg-gray-50 border-none" />
-      </div>
+      {showAll && (
+        <div className="mb-5">
+          <Input
+            placeholder={searchInputPlaceholder}
+            onChange={(e) => handleSearchChange(e)}
+            className="bg-gray-50 border-none"
+          />
+        </div>
+      )}
       <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
-        {items.map((item, index) => (
+        {list.map((item, index) => (
           <FilterCheckbox
             onCheckedChange={(ids) => console.log(ids)}
             checked={false}
@@ -45,6 +60,14 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
           />
         ))}
       </div>
+
+      {items.length > limit && (
+        <div className={showAll ? 'border-t border-t-neutral-100 mt-4' : ''}>
+          <button onClick={() => setShowAll(!showAll)} className="text-primary mt-3">
+            {showAll ? 'Schließen' : 'Mehr anzeigen'}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
