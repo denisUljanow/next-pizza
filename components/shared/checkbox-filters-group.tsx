124acdd189b9adf804f0387bd78d7c0a8f6312e2
denisUljanow <dul@silbergrau.com>
2025-10-27 12:57:21 +0100

    finished ingridient-skeleton add

--- components/shared/checkbox-filters-group.tsx
+++ components/shared/checkbox-filters-group.tsx
@@ -38,12 +38,18 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
   };
 
   if (loading) {
-    return <div className="flex items-center space-x-4">
+    return (
+      <div>
+        {[...Array(limit)].fill(0).map((_, index) => (
+          <div key={index} className="flex items-center space-x-4">
             <Skeleton className="h-12 w-12 rounded-full" />
-              <div className="space-y-2">
-                <Skeleton className="h-4 w-[250px]"/>
-              </div>
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px] rounded-md" />
             </div>
+          </div>
+        ))}
+      </div>
+    );
   };
 
   const list = showAll
