04cc8cde0ae22b0dd1a0fa6c47bf7db8187c212c
denisUljanow <d.ulyanov@yahoo.de>
2026-06-30 12:07:23 +0200

    Empty-cart added

--- shared/components/shared/cart-drawer.tsx
+++ shared/components/shared/cart-drawer.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import Image from 'next/image';
 import { Button } from "@/shared/components/ui/button";
 import {
   Sheet,
@@ -13,12 +14,13 @@ import {
   SheetTrigger,
 } from "@/shared/components/ui/sheet";
 import Link from 'next/link';
-import { ArrowRight } from 'lucide-react';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
 import { CartDrawerItem } from './cart-drawer-item';
-import { getCartItemDetails } from '@/shared/lib';
+import { cn, getCartItemDetails } from '@/shared/lib';
 import { useCartStore } from '@/app/store/cart';
 import { PizzaSizeKey, PizzaTypeKey } from '@/shared/constants/pizza';
 import { Ingredient } from '@prisma/client';
+import { Title } from './title';
 
 interface Props {
 className?: string;
@@ -41,17 +43,39 @@ export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({ children,
   React.useEffect(() => {
     fetchCartItems();
   }, [fetchCartItems]);
-  
+  /////////////////////////////////
   return (
     <Sheet>
         <SheetTrigger asChild>{children}</SheetTrigger>
         <SheetContent className="flex flex-col justify-between pb-0 bg-[#F4F1EE]">
-          <SheetHeader>
+          <div className={cn('flex flex-col h-full', !totalAmount && 'justify-center')}>
+          {totalAmount > 0 && (
+            <SheetHeader>
             <SheetTitle>
                 Im Warenkorb <span className="font-bold">{items.length} Waren</span>
             </SheetTitle>
           </SheetHeader>
+        )}
         
+        {!totalAmount && (
+          <div className="flex flex-col items-center justify-center w-72 mx-auto">
+            <Image src="/assets/empty-box.png" alt="Empty cart" width={120} height={120} />
+            <Title size="sm" text="Dein Warenkorb ist leer" className="text-center font-bold my-2" />
+            <p className="text-center text-neutral-500 mb-5">
+              Füge Produkte hinzu, um sie hier zu sehen.
+            </p>
+
+            <SheetClose>
+              <Button className="w-56 h-12 text-base" size="lg">
+                <ArrowLeft className="w-5 mr-2" />
+                Zurück
+              </Button>
+            </SheetClose>
+          </div>   
+        )}
+
+        {totalAmount > 0 && (
+          <>
             <div className="-mx-6 mt-5 overflow-auto flex-1">
               {items.map((item) => (
                 <div key={item.id} className="mb-2">
@@ -75,7 +99,7 @@ export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({ children,
               ))}
             </div>
 
-            <SheetFooter className="-mx-6 bg-white p-8">
+              <SheetFooter className="-mx-6 bg-white p-8">
                 <div className="w-full">
                     <div className="flex mb-4">
                         <span className="flex flex-1 text-lg text-neutral-500">
@@ -95,6 +119,9 @@ export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({ children,
                     </Link>
                 </div>
             </SheetFooter>
+          </>
+        )} 
+        </div>
         </SheetContent>
     </Sheet>
   );
